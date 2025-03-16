ac09b4696e5969441704001a46b55c14a939c9ab
Robert Eikmanns <robert.eikmanns@stud.htwk-leipzig.de>
2025-03-16 14:50:43 +0100

    use boolean return value to stop the algorithm, check wether an observer is registered to the solver

--- src/main/java/SudokuSolver/Algorithm/Solver.java
+++ src/main/java/SudokuSolver/Algorithm/Solver.java
@@ -17,27 +17,25 @@ public class Solver {
     public void registerGUIobserver(GUIobserver observer) {
         this.observer = observer;
     }
-    
 
     // recursive method for solving a given sudoku from its starting position.
-    public void solveSudoku(final int n, boolean visualize) {
+    public boolean solveSudoku(final int n, boolean visualize) {
         // as soon as we are at the end of our game field, we should have a solution worth seeing.
         if(n > 80) {
             field.printField();
-            observer.freeze();
 
-            return;
+            return true;
         }
 
         // if the field we are working on has a number bigger than zero
         // it is a given number we are not allowed to change. In this case we simply continue with our recursion.
         if(field.getNumber(n) > 0) {
-            solveSudoku(n + 1, visualize);
+            return solveSudoku(n + 1, visualize);
         } else {
             // let's try all nine possible numbers for our field.
             for(int i = 1; i < 10; i++) {
                 field.setNumber(n, i);
-                observer.informChangedField(n);
+                if (observer != null) observer.informChangedField(n);
 
                 if (visualize) {
                     try { Thread.sleep(100); } catch (InterruptedException e) {}
@@ -46,17 +44,19 @@ public class Solver {
                 // we need to check if the number is valid.
                 // if so we might have the right number and continue with the recursion.
                 if(!field.inRow(n) && !field.inColumn(n) && !field.inBox(n)) {
-                    solveSudoku(n + 1, visualize);
+                    if(solveSudoku(n + 1, visualize)) return true;
                 }
             }
 
             // if no number led to a correct solution we leave this recursion call
             // and reset the value of our field to zero.
             field.setNumber(n, 0);
-            observer.informChangedField(n);
+            if (observer != null) observer.informChangedField(n);
             if (visualize) {
                 try { Thread.sleep(100); } catch (InterruptedException e) {}
             }
+
+            return false;
         }
     }
 }
